edb56b67196a11e2658c9a3591086a46633adaa2
codewithsanju <sanjeevkumar919890@gmail.com>
2023-10-16 14:41:26 +0530

    stack_memory_or_heap_memory.js 16/10/2023

---
+++ 01_basics/stack-heap.js
@@ -0,0 +1,5 @@
+// Stack (Primitive), Heap (Non-Primitive)
+
+// stack Memory :- stack memory me jo bhi data dete hai uska ak copy banta hai..
+
+// Heap memory :- heap memory me ho bhi data deta hai uska original value change hota hai..

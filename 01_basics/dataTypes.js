266bac98386a4aca379d9bbb0bef9db8882db026
codewithsanju <sanjeevkumar919890@gmail.com>
2023-10-16 12:55:51 +0530

    16/10/2023

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version.
+
+// alert (3+3) // we are using nodejs, not browser.
+
+console.log(3 
+    + 3); // code readability should be high.
+
+    //tc39.es// javascript documentaions.
+    //mdnjavascript// javascript documentions.
+
+    let name = "sanjeev";
+    let age = 18;
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+    // object 
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const gmail = "gamerplut@gmail.com";
+let accemail = "admin@gmail.com";
+var accpass = "12345";
+acc = "Jaipur";
+
+// gmail = "sanjeev@gmail.com"; // Not Allowed
+
+accemail = "abc@gmail.com";
+accpass = "54321";
+acc = "Bihar"
+console.log(gmail);
+console.table([accemail,accpass,acc]);
